e90cd1b43d98a13149ce0ffd69e927744e1976b3
SoumiaSAADOUN <es_saadoun@esi.dz>
2021-07-12 11:25:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './styles/App.css';
+import Header from './components/Header';
+import Social from './components/Social';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+             <Header />
+             <p> Bonjour tout le monde, aujourd'hui nous somme le 12 juillet 2021</p>
+             <Social/>
     </div>
   );
 }
---
+++ src/components/Social.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Social (){
+    return(<ul>
+        <li>Facebook</li>
+        <li>Instagrame</li>
+        <li>Twitetr</li>      
+        </ul>);
+} 
+
+export default Social;
